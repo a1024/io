e535075d3f6e00946cfa3e9ff438d70e0991bdf7
Ayman Wagih <aymanwmxyz@gmail.com>
2022-06-12 13:56:00 +0200

    GPLv3

--- io.c
+++ io.c
@@ -1,3 +1,19 @@
+//io.c - A cross-platform window creation, event handling and immediate graphics library
+//Copyright (C) 2022  Ayman Wagih, unless source link provided
+//
+//This program is free software: you can redistribute it and/or modify
+//it under the terms of the GNU General Public License as published by
+//the Free Software Foundation, either version 3 of the License, or
+//(at your option) any later version.
+//
+//This program is distributed in the hope that it will be useful,
+//but WITHOUT ANY WARRANTY; without even the implied warranty of
+//MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//GNU General Public License for more details.
+//
+//You should have received a copy of the GNU General Public License
+//along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 #define				IO_IMPLEMENTATION
 #include			"io.h"
 #include			<stdio.h>
