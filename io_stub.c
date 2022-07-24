aca24c8a0bd604dab4a0db6cd17e415b840fc1b4
Ayman Wagih <aymanwmxyz@gmail.com>
2022-07-24 17:55:44 +0200

    Cleanup io_stub.c

--- io_stub.c
+++ io_stub.c
@@ -187,33 +187,9 @@ void io_timer()
 	if(keyboard[KEY_ENTER])	cam_zoomIn(cam, 1.1f);
 	if(keyboard[KEY_BKSP])	cam_zoomOut(cam, 1.1f);
 }
-static void draw_line_3d(float *p1, float *p2, int color)
-{
-	float cp[3], temp1[3], s1[2], s2[2];
-	int X0=w>>1, Y0=h>>1;
-
-	cam_world2cam(cam, p1, cp, temp1);
-	if(cp[2]<0)
-		return;
-	cam_cam2screen(cam, cp, s1, X0, Y0);
-
-	cam_world2cam(cam, p2, cp, temp1);
-	if(cp[2]<0)
-		return;
-	cam_cam2screen(cam, cp, s2, X0, Y0);
-
-	draw_line(s1[0], s1[1], s2[0], s2[1], color);
-}
 void io_render()
 {
 	prof_add("entry");
-/*	static float level=0, delta=0.01f;
-	glClearColor(level, level, level, 1);
-	level+=delta;
-	if(level>1)
-		level=1, delta=-0.01f;
-	else if(level<0)
-		level=0, delta=0.01f;//*/
 	glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
 
 	if(!h)
@@ -226,61 +202,14 @@ void io_render()
 		0, 1, 0,
 		0, 0, 1,
 	};
-	draw_line_3d(axes, axes+1, 0xFF0000FF);
-	draw_line_3d(axes, axes+2, 0xFF00FF00);
-	draw_line_3d(axes, axes+3, 0xFFFF0000);
-	//if(wireframe)
-	//	glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
-	//draw_L3D(&cam, &gpu_teapot, modelPos, lightPos, 0x80C0FF);
-	//if(wireframe)
-	//	glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+	draw_3d_line(&cam, axes, axes+3, 0xFF0000FF);
+	draw_3d_line(&cam, axes, axes+6, 0xFF00FF00);
+	draw_3d_line(&cam, axes, axes+9, 0xFFFF0000);
 	prof_add("model");
 
-#ifdef DEBUG_TEAPOT
-	int wireColor=0xFF80FF80;
-	auto vert=cpu_teapot.VVVNNNTT;
-	auto indices=cpu_teapot.indices;
-	for(int kt=0;kt<cpu_teapot.n_elements;kt+=3)
-	{
-		float tr[]=
-		{
-			vert[indices[kt]<<3], vert[indices[kt]<<3|1], vert[indices[kt]<<3|2],
-			vert[indices[kt+1]<<3], vert[indices[kt+1]<<3|1], vert[indices[kt+1]<<3|2],
-			vert[indices[kt+2]<<3], vert[indices[kt+2]<<3|1], vert[indices[kt+2]<<3|2],
-		};
-		draw_line_3d(tr, tr+3, wireColor);
-		draw_line_3d(tr+3, tr+6, wireColor);
-		draw_line_3d(tr+6, tr, wireColor);
-	}
-#endif
 	GUIPrint(0, 0, 0, 1, "p(%f, %f, %f) a(%f, %f) fov %f", cam.x, cam.y, cam.z, cam.ax, cam.ay, atan(cam.tanfov)*todeg*2);
 	GUIPrint(0, 0, tdy, 1, "timer %d, rand %d", timer, rand());
-//	float y=tdy*3;
-//	if(openfiles)
-//	{
-//		GUIPrint(0, 0, y, 1, "Open files:");
-//		y+=tdy;
-//		for(int k=0, count=array_size(&openfiles);k<count;++k)
-//		{
-//			GUIPrint(0, 0, y, 1, "%d\t%s", k, *(char**)array_at(&openfiles, k));
-//			y+=tdy;
-//		}
-//	}
-//	if(savedfile)
-//		GUIPrint(0, 0, y, 1, "Saved as: %s", savedfile);
-#if 0
-	for(int k=0;k<h;k+=2)
-		draw_line((float)(w>>1), (float)k, (float)(w*3>>2), (float)k, 0xFFFF00FF);
-	draw_ellipse(0, (float)(w>>2), 0, (float)(h>>2), 0xFF0000FF);
-	draw_ellipse((float)(w>>2), (float)(w>>1), (float)(h>>2), (float)(h>>1), 0xFFFF0000);
-	for(int k=0;k<1;++k)
-	{
-		int color=0xFF000000|rand()<<15|rand();
-		draw_rectangle_hollow((float)(rand()%w), (float)(rand()%w), (float)(rand()%h), (float)(rand()%h), color);
-		draw_ellipse((float)(rand()%w), (float)(rand()%w), (float)(rand()%h), (float)(rand()%h), color);
-	}
-	draw_line_i(rand()%w, rand()%h, rand()%w, rand()%h, 0xFF000000|rand()<<15|rand());
-#endif
+
 	prof_add("finish");
 }
 int io_quit_request()//return 1 to exit
